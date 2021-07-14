36810880902f929f92dfc56309345e8061b54fb7
pradiptak <pradiptak@MacBook-Pro-6.local>
2021-07-14 19:13:31 +0530

    data file for user guide page

---
+++ src/topics.js
@@ -0,0 +1,34 @@
+const topics = [
+  {
+    heading: "User Guide - A",
+    description: "A-Here are what some of our amazing customers are saying about our hotels & tours. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    steps: [
+      {
+        imageSrc:
+        "https://images.unsplash.com/photo-1512100356356-de1b84283e18?ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&q=80",
+        quote: "Some random text"
+      },
+      {
+        imageSrc:
+        "https://images.unsplash.com/photo-1512100356356-de1b84283e18?ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&q=80",
+        quote: "Some random text B"
+      }]
+  },
+  {
+    heading: "User Guide -B",
+    description: "A-Here are what some of our amazing customers are saying about our hotels & tours. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    steps: [
+      {
+        imageSrc:
+        "https://images.unsplash.com/photo-1512100356356-de1b84283e18?ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&q=80",
+        quote: "Some random text for topic B"
+      },
+      {
+        imageSrc:
+        "https://images.unsplash.com/photo-1512100356356-de1b84283e18?ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&q=80",
+        quote: "Some random text step 2B"
+      }]
+  }
+];
+
+export default topics;
