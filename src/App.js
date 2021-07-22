c729044e0b09d8ee668a46e82a68b048086320c4
Vivek Singh <petmongrels@gmail.com>
2021-07-22 19:29:19 +0530

    removed comments

--- src/App.js
+++ src/App.js
@@ -1,79 +1,39 @@
 import "tailwindcss/dist/base.css";
 import "styles/globalStyles.css";
 import React from "react";
-import { css } from "styled-components/macro"; //eslint-disable-line
 
-/*
- * This is the entry point component of this project. You can change the below exported default App component to any of
- * the prebuilt landing page components by uncommenting their import and export lines respectively.
- * See one of the landing page components to better understand how to import and render different components (Always
- * make sure if you are building your own page, the root component should be the AnimationRevealPage component. You can
- * disable the animation by using the disabled prop.
- *
- * The App component below is using React router to render the landing page that you see on the live demo website
- * and the component previews.
- *
- */
+import UserGuide from "demos/UserGuide.js";
+import FAQ from "components/faqs/SingleCol.js";
+import DownloadApp from "components/cta/DownloadApp.js";
+import Mobile from "demos/MobilePage.js";
+import Dashboard from "demos/DashboardPage.js";
 
-/* Use AnimationRevealPage as a wrapper component for your pages if you are building a custom one yourself */
- import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-
-/*
- * Hero section is the top most section on the page. It contains the header as well.
- * So you dont need to import headers
- * separately
- */
-
- import UserGuide from "demos/UserGuide.js";
- import FAQ from "components/faqs/SingleCol.js";
- import DownloadApp from "components/cta/DownloadApp.js";
- import Mobile from "demos/MobilePage.js";
- import Dashboard from "demos/DashboardPage.js";
-
-
-/* Ready Made Pages (from demos folder) */
 import LandingPage from "demos/LandingPage.js";
-
-/* Inner Pages */
-// import LoginPage from "pages/Login.js";
-// import SignupPage from "pages/Signup.js";
-// import PricingPage from "pages/Pricing.js";
-// import AboutUsPage from "pages/AboutUs.js";
-// import ContactUsPage from "pages/ContactUs.js";
-// import BlogIndexPage from "pages/BlogIndex.js";
-// import TermsOfServicePage from "pages/TermsOfService.js";
-// import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
-
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 export default function App() {
-  // If you want to disable the animation just use the disabled `prop` like below on your page's component
-  // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
-
-
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/faqs">
-            <FAQ />
-        </Route>
-        <Route exact path="/docs">
-            <UserGuide />
-        </Route>
-        <Route exact path="/download">
-            <DownloadApp />
-        </Route>
-        <Route exact path="/mobile">
-            <Mobile />
-        </Route>
-        <Route exact path="/dashboard">
-            <Dashboard />
-        </Route>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/faqs">
+                    <FAQ/>
+                </Route>
+                <Route exact path="/docs">
+                    <UserGuide/>
+                </Route>
+                <Route exact path="/download">
+                    <DownloadApp/>
+                </Route>
+                <Route exact path="/mobile">
+                    <Mobile/>
+                </Route>
+                <Route exact path="/dashboard">
+                    <Dashboard/>
+                </Route>
+                <Route exact path="/">
+                    <LandingPage/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
