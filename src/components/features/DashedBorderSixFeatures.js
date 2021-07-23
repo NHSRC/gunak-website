41060817e0b46c0c536b977695ea4131f093c0ae
Vivek Singh <petmongrels@gmail.com>
2021-07-23 18:17:36 +0530

    supported programs

--- src/components/features/DashedBorderSixFeatures.js
+++ src/components/features/DashedBorderSixFeatures.js
@@ -2,12 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 //eslint-disable-next-line
-import { css } from "styled-components/macro";
-import { SectionHeading } from "components/misc/Headings.js";
+import {css} from "styled-components/macro";
+import {SectionHeading} from "components/misc/Headings.js";
 
 import defaultCardImage from "../../images/shield-icon.svg";
 
-import { ReactComponent as SvgDecoratorBlob3 } from "../../images/svg-decorator-blob-3.svg";
+import {ReactComponent as SvgDecoratorBlob3} from "../../images/svg-decorator-blob-3.svg";
 
 import SupportIconImage from "../../images/support-icon.svg";
 import ShieldIconImage from "../../images/shield-icon.svg";
@@ -30,9 +30,9 @@ const Column = styled.div`
 const Card = styled.div`
   ${tw`flex flex-col mx-auto max-w-xs items-center px-6 py-10 border-2 border-dashed border-primary-500 rounded-lg mt-12`}
   .imageContainer {
-    ${tw`border-2 border-primary-500 text-center rounded-full p-6 flex-shrink-0 relative`}
+    ${tw`border-2 border-primary-500 text-center rounded-full p-10 flex-shrink-0 relative`}
     img {
-      ${tw`w-8 h-8`}
+      ${tw`w-40 h-40`}
     }
   }
 
@@ -53,49 +53,28 @@ const DecoratorBlob = styled(SvgDecoratorBlob3)`
   ${tw`pointer-events-none absolute right-0 bottom-0 w-64 opacity-25 transform translate-x-32 translate-y-48 `}
 `;
 
-export default () => {
-  /*
-   * This componets has an array of object denoting the cards defined below. Each object in the cards array can have the key (Change it according to your need, you can also add more objects to have more cards in this feature component):
-   *  1) imageSrc - the image shown at the top of the card
-   *  2) title - the title of the card
-   *  3) description - the description of the card
-   *  If a key for a particular card is not provided, a default value is used
-   */
-
-  const cards = [
-    {
-      imageSrc: ShieldIconImage,
-      title: "Ads Management",
-      description: "We create and manage ads that you need, from creation to deployment. Lorem ipsum donor sit amet consicou.Lorem ipsum donor sit amet consicou.Lorem ipsum donor sit amet consicou.Lorem ipsum donor sit amet consicou"
-    },
-    { imageSrc: SupportIconImage, title: "Video Marketing" },
-    { imageSrc: CustomizeIconImage, title: "Customer Relation" }
-    // { imageSrc: ReliableIconImage, title: "Product Outreach" },
-    // { imageSrc: FastIconImage, title: "PR Campaign" },
-    // { imageSrc: SimpleIconImage, title: "Product Expansion" }
-  ];
-
-  return (
-    <Container>
-      <ThreeColumnContainer>
-        <Heading>Programs supported by <span tw="text-primary-500">Gunak</span></Heading>
-        {cards.map((card, i) => (
-          <Column key={i}>
-            <Card>
+export default ({cards}) => {
+    return (
+        <Container>
+            <ThreeColumnContainer>
+                <Heading>Programs supported by <span tw="text-primary-500">Gunak</span></Heading>
+                {cards.map((card, i) => (
+                    <Column key={i}>
+                        <Card>
               <span className="imageContainer">
-                <img src={card.imageSrc || defaultCardImage} alt="" />
+                <img src={card.imageSrc || defaultCardImage} alt=""/>
               </span>
-              <span className="textContainer">
-                <span className="title">{card.title || "Fully Secure"}</span>
+                <span className="textContainer">
+                    <a href={card.link} target="_blank"><span className="title">{card.title}</span></a>
                 <p className="description">
-                  {card.description || "Lorem ipsum donor amet siti ceali ut enim ad minim veniam, quis nostrud. Sic Semper Tyrannis. Neoas Calie artel."}
+                  {card.description}
                 </p>
               </span>
-            </Card>
-          </Column>
-        ))}
-      </ThreeColumnContainer>
-      <DecoratorBlob />
-    </Container>
-  );
+                        </Card>
+                    </Column>
+                ))}
+            </ThreeColumnContainer>
+            <DecoratorBlob/>
+        </Container>
+    );
 };
--- src/demos/LandingPage.js
+++ src/demos/LandingPage.js
@@ -8,27 +8,43 @@ import Features from "components/features/ThreeColWithSideImage.js";
 import Technology from "components/cards/PortfolioTwoCardsWithImage.js"
 import Footer from "components/footers/SimpleFiveColumn.js";
 import gunakFeatures from "../gunakfeatures.js"
+import ShieldIconImage from "../images/nqas.png";
+import Laqshya from "../images/laqshya.jpg";
+import CustomizeIconImage from "../images/kayakalp.png";
+
+const cards = [
+    {
+        imageSrc: ShieldIconImage,
+        title: "NQAS",
+        description: "National Quality Assurance Standards",
+        link: "http://qi.nhsrcindia.org/national-quality-assurance-standards"
+    },
+    {imageSrc: Laqshya, title: "LAQSHYA", description: "Labour Room Quality Improvement Initiative", link: "http://nhm.gov.in/New_Updates_2018/NHM_Components/RMNCH_MH_Guidelines/LaQshya-Guidelines.pdf"},
+    {imageSrc: CustomizeIconImage, title: "KAYAKALP", description: "Swacchta Guidelines for Public Health Facilities", link: "https://www.nhp.gov.in/kayakalp-swacchta-guidelines-for-public-health-facilities_pg"}
+];
 
 export default () => {
-  const Subheading = tw.span`uppercase tracking-widest font-bold text-primary-500`;
-  const HighlightedText = tw.span`text-primary-500`;
+    const Subheading = tw.span`uppercase tracking-widest font-bold text-primary-500`;
+    const HighlightedText = tw.span`text-primary-500`;
 
-  return (
-    <AnimationRevealPage disabled>
-      <Header />
-      <Hero primaryButtonUrl="/mobile" heading={"Gunak - National health facilities assessment platform"} description={"Designed for all hospital administration, state health department, and quality assessors"} primaryButtonText={"Download Mobile App"} watchVideoButtonText={"How it Works"}/>
-      <Programs />
-      <Features
-        subheading={<Subheading>Features</Subheading>}
-        heading={
-          <>
-            We have Amazing <HighlightedText>Service.</HighlightedText>
-          </>
-        }
-        cards={gunakFeatures}
-      />
-      <Technology />
-      <Footer />
-    </AnimationRevealPage>
-  );
+    return (
+        <AnimationRevealPage disabled>
+            <Header/>
+            <Hero primaryButtonUrl="/mobile" heading={"Gunak - National health facilities assessment platform"}
+                  description={"Designed for all hospital administration, state health department, and quality assessors"} primaryButtonText={"Download Mobile App"}
+                  watchVideoButtonText={"How it Works"}/>
+            <Programs cards={cards}/>
+            <Features
+                subheading={<Subheading>Features</Subheading>}
+                heading={
+                    <>
+                        We have Amazing <HighlightedText>Service.</HighlightedText>
+                    </>
+                }
+                cards={gunakFeatures}
+            />
+            <Technology/>
+            <Footer/>
+        </AnimationRevealPage>
+    );
 }
